fe003f8fdf9054f2b0bbc4d2a001a5cd8e6d5eff
bilal <bilal@DESKTOP-OVA44RI>
2021-06-03 13:43:41 +0200

    test

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 app.use("/api/users", userRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/dist/src/index.html'));
+});
 
 app.listen(process.env.PORT || process.env.APP_PORT,() => {
   console.log("server up and running on port : ", process.env.PORT || process.env.APP_PORT);
