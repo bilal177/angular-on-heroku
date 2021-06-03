012b33c0b09f547b73ec96198a8d00a34867ad8e
bilal <bilal@DESKTOP-OVA44RI>
2021-06-03 14:45:20 +0200

    test

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express();
 const userRouter = require("./api/users/user.router")
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/Dalstock'));
+app.use(express.static(__dirname + 'dist/Dalstock'));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/Dalstock/index.html'));
 });
-
+app.use("/api/users", userRouter);
 app.listen(process.env.PORT || process.env.APP_PORT,() => {
   console.log("server up and running on port : ", process.env.PORT || process.env.APP_PORT);
 });
