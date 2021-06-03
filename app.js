4e118b702c675a757f3b7a25a36176936d121224
bilal <bilal@DESKTOP-OVA44RI>
2021-06-03 12:40:00 +0200

    test

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router")
 
-app.use(express.json());
+if (process.env.NODE_ENV === "production"{
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("/src",  "build", "index.html"));
+  });
+}else{
+  app.use(express.json());
+}
+
 app.use("/api/users", userRouter);
 
 app.listen(process.env.PORT || process.env.APP_PORT,() => {
-  console.log("server up and running on port : ", process.env.APP_PORT);
+  console.log("server up and running on port : ", process.env.PORT || process.env.APP_PORT);
 });
