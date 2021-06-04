95ef9467959caccbddfb6681d415446cf5f55f28
bilal yildirim <bilal@dalcom.be>
2021-06-04 21:39:54 +0200

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true
 };
 export const baseUrl=`https://dalstock-angular.herokuapp.com/api/`;
 export const baseUrlLive=`https://localhost:4200/api/`;
