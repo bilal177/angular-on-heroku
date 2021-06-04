1624153dd98ee4a11e12c577a1ee9f1f300e7703
bilal yildirim <bilal@dalcom.be>
2021-06-04 10:35:09 +0200

    test

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 const userRouter = require("./api/users/user.router")
 const path = require('path');
 app.use(express.json());
-app.use(express.static(__dirname + 'dist/Dalstock'));
+app.use(express.static(__dirname + 'dist/Dalstock', {index: false}));
 app.use("/api/users", userRouter);
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/Dalstock/index.html'));
+  res.sendFile(path.join(__dirname,'src', 'index.html'));
 });
 
 
