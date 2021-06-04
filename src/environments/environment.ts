cf3ffdfeaed12fdd8c3c8f3e9860b7f90b5490c4
bilal yildirim <bilal@dalcom.be>
2021-06-04 21:49:16 +0200

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: false
 };
-export const baseUrl=`https://dalstock-angular.herokuapp.com/api/`;
-export const baseUrlLive=`https://localhost:4200/api/`;
+export const baseUrl=`https://localhost:4200/api/`;
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
