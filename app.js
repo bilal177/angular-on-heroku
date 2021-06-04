b9dc5d9dbb3cf6af671fec6c2818273b3bde8469
bilal yildirim <bilal@dalcom.be>
2021-06-04 10:21:51 +0200

    test

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router")
 const path = require('path');
-
+app.use(express.json());
 app.use(express.static(__dirname + 'dist/Dalstock'));
-
+app.use("/api/users", userRouter);
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/Dalstock/index.html'));
 });
-app.use(express.json());
-app.use("/api/users", userRouter);
+
+
 app.listen(process.env.PORT || process.env.APP_PORT,() => {
   console.log("server up and running on port : ", process.env.PORT || process.env.APP_PORT);
 });
