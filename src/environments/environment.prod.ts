07e18c138a1db0deb29f4c32091432989e32dc65
bilal yildirim <bilal@dalcom.be>
2021-06-05 00:01:06 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true
 };
-export const baseUrl=`http://dalstock-angular.herokuapp.com/api/`;
+export const baseUrl=`https://dalstock-angular.herokuapp.com/api/`;
